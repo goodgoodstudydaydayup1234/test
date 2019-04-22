024ab1e986037cbed7c5e25abb0ad94e12e3d3ff
xcy <2698064198@qq.com>
2019-04-22 21:54:30 +0800

    首页

--- computer3/computer3/urls.py
+++ computer3/computer3/urls.py
@@ -19,6 +19,6 @@ from django.conf.urls import url, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^user/', include('df_user.urls')),
-    url(r'^goods', include('df_goods.urls')),
+    url(r'user/', include('df_user.urls')),
+    url(r'goods/', include('df_goods.urls')),
 ]
--- computer3/df_goods/views.py
+++ computer3/df_goods/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from .models import Taishiji,Bijiben,Zhuban,Xianka,Cpu,Neicun,Yingpan,Jixiang,Yjxsq,Yinxiang,Shubiao,Jianpan
 # Create your views here.
+
+
+def index(request):
+    goods = Cpu.objects.all()
+    print(goods)
+    return render(request, 'df_goods/index.html', {'goods': goods})
+
+
+
+
+
+
+
